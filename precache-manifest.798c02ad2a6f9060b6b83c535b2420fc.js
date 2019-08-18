d107efa613720b00897214a8a56ba018ff9f653f
Rajiv Venugopal <rajivgopal@yahoo.com>
2019-08-18 21:53:43 +0800

    Updates

---
+++ precache-manifest.798c02ad2a6f9060b6b83c535b2420fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e55ab411a55ba9c05c6216a809de26",
+    "url": "/flux-tutorial/index.html"
+  },
+  {
+    "revision": "89863388688941c18310",
+    "url": "/flux-tutorial/static/css/2.b62f5c36.chunk.css"
+  },
+  {
+    "revision": "89863388688941c18310",
+    "url": "/flux-tutorial/static/js/2.8887cf60.chunk.js"
+  },
+  {
+    "revision": "06aae18b9b483343ba59",
+    "url": "/flux-tutorial/static/js/main.78230790.chunk.js"
+  },
+  {
+    "revision": "ff1be1ecd7396de7c426",
+    "url": "/flux-tutorial/static/js/runtime~main.2f0d711f.js"
+  }
+]);
